6529ac1a810dd2c6d42ee7b04ea7d1794f5b570f
BenjaminPoblete999 <129112124+Poblete999@users.noreply.github.com>
2023-06-27 10:46:42 -0400

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,14 @@
+Ejercicio 1
+
+N1 = int(input("Ingrese el primer número "))
+N2 = int(input("Ingrese el segundo número "))
+N3 = int(input("Ingrese el tercer número "))
+
+if N1 > N2:
+    N1 > N3
+    print("El número mayor es ", N1)
+else: 
+    if N2 > N3:
+        print("El número mayor es ", N2)
+    else:
+        print("El número mayor es ", N3)
